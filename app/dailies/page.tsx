c2af113b7cdd56052ba6d482303e8ad001849999
Andrew <lolfill0104@gmail.com>
2024-06-22 04:39:55 -0400

    decoupled all methods from timer class

--- app/dailies/page.tsx
+++ app/dailies/page.tsx
@@ -1,135 +1,138 @@
-'use client';
-import { useEffect, useMemo, useState } from 'react';
-import CharacterSelection from '@/app/components/CharacterNameSelection';
-import TaskSection from '@/app/components/taskSection/TaskSection';
-import AddEditModal from '@/app/components/addEditModal/AddEditModal';
-import { DEFAULTUSERDATA, UserData } from '@/app/constants/defaults';
-import Countdown from '@/app/components/Countdown';
-import timeHelper from '@/app/helpers/time';
-import { uncheckTasks } from '@/app/helpers/dataOperations';
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import CharacterSelection from "@/app/components/CharacterNameSelection";
+import TaskSection from "@/app/components/taskSection/TaskSection";
+import AddEditModal from "@/app/components/addEditModal/AddEditModal";
+import { DEFAULTUSERDATA, UserData } from "@/app/constants/defaults";
+import Countdown from "@/app/components/Countdown";
+import {
+  getNextEventInfo,
+  getPreviousMidnightUTC,
+  getUpcomingMidnight,
+} from "@/app/helpers/time";
+import { uncheckTasks } from "@/app/helpers/dataOperations";
 
 const Daiies: React.FC = () => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [userData, setUserData] = useState<UserData>();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userData, setUserData] = useState<UserData>();
 
-	const time = useMemo(() => new timeHelper(), []);
-	const { eventTime, eventName, eventStarting } = time.getNextEventInfo();
+  const { eventTime, eventName, eventStarting } = getNextEventInfo();
 
-	useEffect(() => {
-		const localStorageUserData = userData ?? localStorage.getItem('userData');
+  useEffect(() => {
+    const localStorageUserData = userData ?? localStorage.getItem("userData");
 
-		if (!localStorageUserData) {
-			localStorage.setItem('userData', JSON.stringify(DEFAULTUSERDATA));
-			setUserData(DEFAULTUSERDATA);
-			return;
-		}
-		const parsedUserData: UserData =
-			typeof localStorageUserData === 'string'
-				? JSON.parse(localStorageUserData)
-				: localStorageUserData;
+    if (!localStorageUserData) {
+      localStorage.setItem("userData", JSON.stringify(DEFAULTUSERDATA));
+      setUserData(DEFAULTUSERDATA);
+      return;
+    }
+    const parsedUserData: UserData =
+      typeof localStorageUserData === "string"
+        ? JSON.parse(localStorageUserData)
+        : localStorageUserData;
 
-		// Uncheck all tasks if lastChecked is before last reset and now is past reset
-		const previousReset = time.getPreviousMidnightUTC().getTime();
+    // Uncheck all tasks if lastChecked is before last reset and now is past reset
+    const previousReset = getPreviousMidnightUTC().getTime();
 
-		if (previousReset > new Date(parsedUserData.lastChecked).getTime()) {
-			if (time.getUpcomingMidnight().getDay() === 4) {
-				uncheckTasks(parsedUserData, setUserData, true);
-			} else {
-				uncheckTasks(parsedUserData, setUserData, false);
-			}
-		} else {
-			setUserData(parsedUserData);
-		}
+    if (previousReset > new Date(parsedUserData.lastChecked).getTime()) {
+      if (getUpcomingMidnight().getDay() === 4) {
+        uncheckTasks(parsedUserData, setUserData, true);
+      } else {
+        uncheckTasks(parsedUserData, setUserData, false);
+      }
+    } else {
+      setUserData(parsedUserData);
+    }
 
-		const close = (e: KeyboardEvent) => {
-			if (e.code === 'Escape') {
-				setIsModalOpen(false);
-			}
-		};
-		window.addEventListener('keydown', close);
-		return () => window.removeEventListener('keydown', close);
-	}, [time, userData]);
+    const close = (e: KeyboardEvent) => {
+      if (e.code === "Escape") {
+        setIsModalOpen(false);
+      }
+    };
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, [userData]);
 
-	const updateUserData = (updatedData: UserData) => {
-		setUserData(updatedData);
-	};
+  const updateUserData = (updatedData: UserData) => {
+    setUserData(updatedData);
+  };
 
-	const characterSelectionComponentArray = useMemo(() => {
-		return userData?.characters.map((character, idx) => {
-			return (
-				<CharacterSelection
-					key={`character-${idx}`}
-					name={character.name}
-					selected={character.selected}
-				/>
-			);
-		});
-	}, [userData]);
+  const characterSelectionComponentArray = useMemo(() => {
+    return userData?.characters.map((character, idx) => {
+      return (
+        <CharacterSelection
+          key={`character-${idx}`}
+          name={character.name}
+          selected={character.selected}
+        />
+      );
+    });
+  }, [userData]);
 
-	const selectedCharacterData = useMemo(() => {
-		return userData?.characters.find((character) => character.selected)!;
-	}, [userData]);
+  const selectedCharacterData = useMemo(() => {
+    return userData?.characters.find((character) => character.selected)!;
+  }, [userData]);
 
-	const toggleModalStatus = () => {
-		setIsModalOpen(!isModalOpen);
-	};
+  const toggleModalStatus = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-	return userData === undefined ? null : (
-		<div className='flex justify-center my-2'>
-			<div className='flex flex-col border rounded max-w-task-container min-h-task-content-box px-4'>
-				<div className='flex flex-col w-full'>
-					<div className='flex'>
-						<div className='flex w-1/3 flex-wrap gap-4'>
-							<Countdown
-								style='border flex flex-col rounded items-center px-4 mt-2'
-								endTime={time.getUpcomingMidnight()}
-								updateUserData={updateUserData}
-								userData={userData}
-								type='daily'
-							/>
-							<Countdown
-								style='border flex flex-col rounded items-center px-4 mt-2'
-								endTime={eventTime}
-								updateUserData={updateUserData}
-								userData={userData}
-								type='event'
-								name={eventName}
-								eventStarting={eventStarting}
-							/>
-						</div>
+  return userData === undefined ? null : (
+    <div className='flex justify-center my-2'>
+      <div className='flex flex-col border rounded max-w-task-container min-h-task-content-box px-4'>
+        <div className='flex flex-col w-full'>
+          <div className='flex'>
+            <div className='flex w-1/3 flex-wrap gap-4'>
+              <Countdown
+                style='border flex flex-col rounded items-center px-4 mt-2'
+                endTime={getUpcomingMidnight()}
+                updateUserData={updateUserData}
+                userData={userData}
+                type='daily'
+              />
+              <Countdown
+                style='border flex flex-col rounded items-center px-4 mt-2'
+                endTime={eventTime}
+                updateUserData={updateUserData}
+                userData={userData}
+                type='event'
+                name={eventName}
+                eventStarting={eventStarting}
+              />
+            </div>
 
-						<u className='flex justify-center mt-2 w-1/3'>
-							{selectedCharacterData.name}&#39;s Dailies
-						</u>
-						<button
-							className='border rounded ml-auto h-8 w-32 mt-2'
-							onClick={() => setIsModalOpen(!isModalOpen)}
-						>
-							Add/Edit Tasks
-						</button>
-					</div>
-					<div className='flex justify-center content-between'>
-						{...characterSelectionComponentArray!}
-					</div>
-				</div>
-				<div className='flex flex-wrap justify-center gap-8 mt-2'>
-					<TaskSection
-						taskData={selectedCharacterData.dailies}
-						charId={selectedCharacterData.id}
-						updateData={updateUserData}
-					/>
-				</div>
-			</div>
-			{isModalOpen ? (
-				<AddEditModal
-					toggleModalStatus={toggleModalStatus}
-					selectedCharDataId={selectedCharacterData.id}
-					userData={userData}
-					type='dailies'
-				/>
-			) : null}
-		</div>
-	);
+            <u className='flex justify-center mt-2 w-1/3'>
+              {selectedCharacterData.name}&#39;s Dailies
+            </u>
+            <button
+              className='border rounded ml-auto h-8 w-32 mt-2'
+              onClick={() => setIsModalOpen(!isModalOpen)}
+            >
+              Add/Edit Tasks
+            </button>
+          </div>
+          <div className='flex justify-center content-between'>
+            {...characterSelectionComponentArray!}
+          </div>
+        </div>
+        <div className='flex flex-wrap justify-center gap-8 mt-2'>
+          <TaskSection
+            taskData={selectedCharacterData.dailies}
+            charId={selectedCharacterData.id}
+            updateData={updateUserData}
+          />
+        </div>
+      </div>
+      {isModalOpen ? (
+        <AddEditModal
+          toggleModalStatus={toggleModalStatus}
+          selectedCharDataId={selectedCharacterData.id}
+          userData={userData}
+          type='dailies'
+        />
+      ) : null}
+    </div>
+  );
 };
 
 export default Daiies;
--- app/helpers/time.ts
+++ app/helpers/time.ts
@@ -1,174 +1,177 @@
-export type CountdownType = 'daily' | 'weekly' | 'event';
+export type CountdownType = "daily" | "weekly" | "event";
 
 type NextEvent = {
-	eventTime: Date;
-	eventName: string;
-	eventStarting?: boolean;
+  eventTime: Date;
+  eventName: string;
+  eventStarting?: boolean;
 };
 
-class timeHelper {
-	private today = new Date();
+const getUpcomingMidnight = (): Date => {
+  const today = new Date();
+  // const utcSeconds = today.getUTCSeconds();
+  const tomorrow: Date = new Date(
+    today.getUTCFullYear(),
+    today.getUTCMonth(),
+    today.getUTCDate() + 1
+  ); // Get tomorrow's date
+  tomorrow.setUTCHours(0, 0, 0, 0); // Set time to midnight UTC
 
-	getUpcomingMidnight(): Date {
-		// const utcSeconds = this.today.getUTCSeconds();
-		const tomorrow: Date = new Date(
-			this.today.getUTCFullYear(),
-			this.today.getUTCMonth(),
-			this.today.getUTCDate() + 1
-		); // Get tomorrow's date
-		tomorrow.setUTCHours(0, 0, 0, 0); // Set time to midnight UTC
+  return tomorrow;
+};
 
-		return tomorrow;
-	}
+const getNextThursdayMidnightUTC = (): Date => {
+  const today = new Date();
+  const dayOfWeek: number = today.getUTCDay();
+  const daysUntilThursday: number =
+    dayOfWeek <= 4 ? 4 - dayOfWeek : 11 - dayOfWeek;
+  const nextThursday: Date = new Date(
+    today.getUTCFullYear(),
+    today.getUTCMonth(),
+    today.getUTCDate() + daysUntilThursday
+  );
+  nextThursday.setUTCHours(0, 0, 0, 0);
+  return nextThursday;
+};
 
-	getNextThursdayMidnightUTC(): Date {
-		const dayOfWeek: number = this.today.getUTCDay();
-		const daysUntilThursday: number =
-			dayOfWeek <= 4 ? 4 - dayOfWeek : 11 - dayOfWeek;
-		const nextThursday: Date = new Date(
-			this.today.getUTCFullYear(),
-			this.today.getUTCMonth(),
-			this.today.getUTCDate() + daysUntilThursday
-		);
-		nextThursday.setUTCHours(0, 0, 0, 0);
-		return nextThursday;
-	}
+const formatTime = (remainingTime: number, type: CountdownType): string => {
+  // Convert milliseconds to days, hours, minutes, and seconds
+  const days: number = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  const hours: number = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes: number = Math.floor(
+    (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  const seconds: number = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-	formatTime(remainingTime: number, type: CountdownType): string {
-		// Convert milliseconds to days, hours, minutes, and seconds
-		const days: number = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-		const hours: number = Math.floor(
-			(remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-		);
-		const minutes: number = Math.floor(
-			(remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-		);
-		const seconds: number = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  const formattedTime = {
+    formattedDays: ``,
+    formattedHours: "",
+    formattedMinutes: "",
+    formattedSeconds: "",
+  };
 
-		const formattedTime = {
-			formattedDays: ``,
-			formattedHours: '',
-			formattedMinutes: '',
-			formattedSeconds: '',
-		};
+  if (days && type === "weekly") {
+    formattedTime.formattedDays = `${days}d `;
+  }
 
-		if (days && type === 'weekly') {
-			formattedTime.formattedDays = `${days}d `;
-		}
+  if (hours > 9) {
+    formattedTime.formattedHours = `${hours}h `;
+  } else {
+    formattedTime.formattedHours = `0${hours}h `;
+  }
 
-		if (hours > 9) {
-			formattedTime.formattedHours = `${hours}h `;
-		} else {
-			formattedTime.formattedHours = `0${hours}h `;
-		}
+  if (minutes > 9) {
+    formattedTime.formattedMinutes = `${minutes}m `;
+  } else {
+    formattedTime.formattedMinutes = `0${minutes}m `;
+  }
 
-		if (minutes > 9) {
-			formattedTime.formattedMinutes = `${minutes}m `;
-		} else {
-			formattedTime.formattedMinutes = `0${minutes}m `;
-		}
+  if (seconds > 9) {
+    formattedTime.formattedSeconds = `${seconds}s`;
+  } else {
+    formattedTime.formattedSeconds = `0${seconds}s`;
+  }
 
-		if (seconds > 9) {
-			formattedTime.formattedSeconds = `${seconds}s`;
-		} else {
-			formattedTime.formattedSeconds = `0${seconds}s`;
-		}
+  return (
+    formattedTime.formattedDays +
+    formattedTime.formattedHours +
+    formattedTime.formattedMinutes +
+    formattedTime.formattedSeconds
+  );
+};
 
-		return (
-			formattedTime.formattedDays +
-			formattedTime.formattedHours +
-			formattedTime.formattedMinutes +
-			formattedTime.formattedSeconds
-		);
-	}
+const getNewUTCDate = (now = new Date()) => {
+  const utcDate = new Date(
+    Date.UTC(
+      now.getUTCFullYear(), // Get the current year (UTC)
+      now.getUTCMonth(), // Get the current month (UTC)
+      now.getUTCDate(), // Get the current day of the month (UTC)
+      now.getUTCHours(), // Get the current hour (UTC)
+      now.getUTCMinutes(), // Get the current minute (UTC)
+      now.getUTCSeconds(), // Get the current second (UTC)
+      now.getUTCMilliseconds() // Get the current millisecond (UTC)
+    )
+  );
 
-	getNewUTCDate(now = new Date()) {
-		const utcDate = new Date(
-			Date.UTC(
-				now.getUTCFullYear(), // Get the current year (UTC)
-				now.getUTCMonth(), // Get the current month (UTC)
-				now.getUTCDate(), // Get the current day of the month (UTC)
-				now.getUTCHours(), // Get the current hour (UTC)
-				now.getUTCMinutes(), // Get the current minute (UTC)
-				now.getUTCSeconds(), // Get the current second (UTC)
-				now.getUTCMilliseconds() // Get the current millisecond (UTC)
-			)
-		);
+  return utcDate;
+};
 
-		return utcDate;
-	}
+const getPreviousMidnightUTC = (): Date => {
+  const today = getNewUTCDate();
+  const yesterday = getNewUTCDate(today);
+  yesterday.setUTCDate(today.getUTCDate()); // Set the date to yesterday
+  yesterday.setUTCHours(0, 0, 0, 0); // Set time to midnight UTC
+  return yesterday;
+};
 
-	getPreviousMidnightUTC(): Date {
-		const today = this.getNewUTCDate();
-		const yesterday = this.getNewUTCDate(today);
-		yesterday.setUTCDate(today.getUTCDate()); // Set the date to yesterday
-		yesterday.setUTCHours(0, 0, 0, 0); // Set time to midnight UTC
-		return yesterday;
-	}
+const getNewEndTime = (type: CountdownType) => {
+  switch (type) {
+    case "weekly":
+      return getNextThursdayMidnightUTC();
+    default:
+      return getUpcomingMidnight();
+  }
+};
 
-	getNewEndTime(type: CountdownType) {
-		switch (type) {
-			case 'weekly':
-				return this.getNextThursdayMidnightUTC();
-			default:
-				return this.getUpcomingMidnight();
-		}
-	}
+const getNextEventInfo = (): NextEvent => {
+  const now = getNewUTCDate();
+  const nowToMilliseconds = now.getTime();
+  const bingo1 = structuredClone(now).setUTCHours(1, 0, 0, 0);
+  const bingo2 = structuredClone(now).setUTCHours(17, 0, 0, 0);
+  const rps1 = structuredClone(now).setUTCHours(5, 0, 0, 0);
+  const rps2 = structuredClone(now).setUTCHours(21, 0, 0, 0);
+  const soGongs1 = structuredClone(now).setUTCHours(13, 0, 0, 0);
+  const soGongs2 = structuredClone(now).setUTCHours(23, 0, 0, 0);
 
-	getNextEventInfo(): NextEvent {
-		const now = this.getNewUTCDate();
-		const nowToMilliseconds = now.getTime();
-		const bingo1 = structuredClone(now).setUTCHours(1, 0, 0, 0);
-		const bingo2 = structuredClone(now).setUTCHours(17, 0, 0, 0);
-		const rps1 = structuredClone(now).setUTCHours(5, 0, 0, 0);
-		const rps2 = structuredClone(now).setUTCHours(21, 0, 0, 0);
-		const soGongs1 = structuredClone(now).setUTCHours(13, 0, 0, 0);
-		const soGongs2 = structuredClone(now).setUTCHours(23, 0, 0, 0);
+  const events: { [x: number]: string } = {};
 
-		const events: { [x: number]: string } = {};
+  events[bingo1] = "Bingo";
+  events[bingo2] = "Bingo";
+  events[rps1] = "Rock Paper Scissors";
+  events[rps2] = "Rock Paper Scissors";
+  events[soGongs1] = "So Gongs Treasure";
+  events[soGongs2] = "So Gongs Treasure";
 
-		events[bingo1] = 'Bingo';
-		events[bingo2] = 'Bingo';
-		events[rps1] = 'Rock Paper Scissors';
-		events[rps2] = 'Rock Paper Scissors';
-		events[soGongs1] = 'So Gongs Treasure';
-		events[soGongs2] = 'So Gongs Treasure';
+  let eventTime = Infinity;
 
-		let eventTime = Infinity;
+  for (const [time, name] of Object.entries(events)) {
+    const currTimeToNumber = Number(time);
+    const currDifference = currTimeToNumber - nowToMilliseconds;
 
-		for (const [time, name] of Object.entries(events)) {
-			const currTimeToNumber = Number(time);
-			const currDifference = currTimeToNumber - nowToMilliseconds;
+    if (currDifference >= -60000 && currDifference <= 0) {
+      return {
+        eventStarting: true,
+        eventTime: new Date(currTimeToNumber + 60000),
+        eventName: name,
+      };
+    }
 
-			if (currDifference >= -60000 && currDifference <= 0) {
-				return {
-					eventStarting: true,
-					eventTime: new Date(currTimeToNumber + 60000),
-					eventName: name,
-				};
-			}
+    if (currTimeToNumber > nowToMilliseconds && currTimeToNumber < eventTime) {
+      eventTime = currTimeToNumber;
+    }
+  }
 
-			if (
-				currTimeToNumber > nowToMilliseconds &&
-				currTimeToNumber < eventTime
-			) {
-				eventTime = currTimeToNumber;
-			}
-		}
+  // If reached end of events, return the next earliest event date + 1 day
+  if (eventTime === Infinity) {
+    return {
+      eventTime: new Date(bingo1 + 24 * 60 * 60 * 1000),
+      eventName: "Bingo",
+    };
+  }
 
-		// If reached end of events, return the next earliest event date + 1 day
-		if (eventTime === Infinity) {
-			return {
-				eventTime: new Date(bingo1 + 24 * 60 * 60 * 1000),
-				eventName: 'Bingo',
-			};
-		}
+  return {
+    eventTime: new Date(eventTime),
+    eventName: events[eventTime],
+  };
+};
 
-		return {
-			eventTime: new Date(eventTime),
-			eventName: events[eventTime],
-		};
-	}
-}
-
-export default timeHelper;
+export {
+  getUpcomingMidnight,
+  getNextThursdayMidnightUTC,
+  formatTime,
+  getNewUTCDate,
+  getPreviousMidnightUTC,
+  getNewEndTime,
+  getNextEventInfo,
+};
